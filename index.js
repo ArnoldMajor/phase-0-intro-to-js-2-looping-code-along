035b4721109a60f9f68932463c9bd887d2d4b841
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-04-07 21:48:46 +0300

    Looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ['Charlie', 'Samip', 'Ali'];
+let events = 'birthday';
+
+function writeCards (names, events){
+    let newNames = [];
+    for (let i = 0; i < names.length; i++){
+        let cardMessage = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+        console.log(cardMessage);
+        newNames.push(cardMessage);
+    }
+    return newNames
+}
+
+console.log(writeCards(names, events));
+
+function countDown (){
+    let number = 10
+    while (number >= 0){
+    console.log(number--);
+}
+}
+
+countDown();
